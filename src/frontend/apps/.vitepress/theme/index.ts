2a9053dab18d3a0ae4e7e99bac785c092dec4a4d
shadowlanes <shadow.lanes@gmail.com>
2025-10-25 14:47:29 +0400

    Added BuyMeACoffee link.

--- src/frontend/apps/.vitepress/theme/index.ts
+++ src/frontend/apps/.vitepress/theme/index.ts
@@ -3,12 +3,14 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './styles/vars.css'
+import BuyMeCoffee from './components/BuyMeCoffee.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'layout-bottom': () => h(BuyMeCoffee)
     })
   },
   enhanceApp({ app }) {
